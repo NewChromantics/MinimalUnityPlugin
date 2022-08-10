cf4d3716d168db6b9e612a03ccfb74a2e67bc116
Graham Reeves <graham@grahamreeves.com>
2022-08-10 18:23:30 +0100

    Added a manual dlopen() implmentation with a "c++ source plugin" (!) for #1 works in dev build

--- UnityTestProject/Assets/MinimalTest.cs
+++ UnityTestProject/Assets/MinimalTest.cs
@@ -2,26 +2,42 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Runtime.InteropServices;
 
 public class MinimalTest : MonoBehaviour
 {
     string VersionString;
     GUIStyle style = new GUIStyle();
     
+    [DllImport("__Internal")]
+    private static extern int DlopenTest();
+
     void Start()
     {
+        
         Debug.Log($"MinimalTest start()");
+        
+        try
+        {
+            var Version = DlopenTest();
+            VersionString += $"DlopenTest={Version}";
+        }
+        catch(System.Exception e)
+        {
+            Debug.LogException(e);
+            VersionString += e.Message;
+        }
+        
         try
         {
             Debug.Log($"Minimal version {Minimal.Minimal.VersionThousand}");
             var Version = Minimal.Minimal.VersionThousand;
-            VersionString = $"{Version}";
+            VersionString += $"Minimal Version {Version}";
             }
         catch(System.Exception e)
         {
             Debug.LogException(e);
-            VersionString = e.Message;
+            VersionString += e.Message;
         }
     }
 
